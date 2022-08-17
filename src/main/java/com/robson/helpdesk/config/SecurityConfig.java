7c11eb55d719a44d301d8cf645e53a664aca60c3
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-17 14:56:14 -0300

    feat: add spring security

---
+++ src/main/java/com/robson/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.robson.helpdesk.config;
+
+import java.util.Arrays;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+	
+	private static final String[] PUBLIC_MATCHERS = {"/h2/**"};
+
+	@Autowired
+	private Environment env;
+	
+	  @Bean
+      public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		  if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			 http.headers().frameOptions().disable(); 
+		  }
+		  
+          http.cors().and().csrf().disable();
+          http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+          http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+          
+          return http.build();
+      }
+	  
+	  @Bean
+	  CorsConfigurationSource configurationSource() {
+		  CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		  configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		  final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		  source.registerCorsConfiguration("/**", configuration);
+		  
+		  return source;
+	  }
+}
