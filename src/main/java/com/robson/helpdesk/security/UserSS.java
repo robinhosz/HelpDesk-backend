804c23bb10381d345057a2bd5b7788a5bdc2407f
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-17 15:07:34 -0300

    feat: final class UserSS

--- src/main/java/com/robson/helpdesk/security/UserSS.java
+++ src/main/java/com/robson/helpdesk/security/UserSS.java
@@ -1,27 +1,52 @@
 package com.robson.helpdesk.security;
 
 import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.robson.helpdesk.enums.Perfil;
 
 public class UserSS implements UserDetails {
 
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getValue()))
+				.collect(Collectors.toSet());
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
 
-		return null;
+		return senha;
 	}
 
 	@Override
 	public String getUsername() {
 
-		return null;
+		return email;
 	}
 
 	@Override
