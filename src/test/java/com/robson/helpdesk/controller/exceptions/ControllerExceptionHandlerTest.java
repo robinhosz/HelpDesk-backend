464ffc77e19439bd120af85525802c1517c98796
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-18 16:09:51 -0300

    feat: init unit test exceptions handlers

---
+++ src/test/java/com/robson/helpdesk/controller/exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,29 @@
+package com.robson.helpdesk.controller.exceptions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerExceptionHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listaDeErrors() {
+    }
+
+    @Test
+    void objectNotFoundException() {
+    }
+
+    @Test
+    void dataIntegrity() {
+    }
+
+    @Test
+    void entityNotFound() {
+    }
+}
